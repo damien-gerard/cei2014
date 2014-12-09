49bf94d23f2e933edcb7078f884daf7c7ce3d228
Florian Lemaitre <florian@lemaitre.re>
2014-12-09 23:13:11 +0100

    Fix du chemin pour le script de compil Windows

--- compile-llvm.bat
+++ compile-llvm.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir llvm-build
 cd llvm-src
-sh ./configure --prefix="%CD:\=/%"
+sh ./configure --prefix="%CD:\=/%/../llvm-build"
 make all CXXFLAGS=-D_GLIBCXX_HAVE_FENV_H
 xcopy include ..\llvm-build\include /s /e /k /p /i
 make install
