1868c08c13653c2c243ee5a715f89e98149f9ff9
AubryH <aubry.herve@supelec.fr>
2014-12-15 19:46:36 +0100

    main can now take testfile in argument exemple : 4dc.exe "test1.4d"

--- 4dc/main.cpp
+++ 4dc/main.cpp
@@ -2,21 +2,38 @@
 
 #include "include/token.h"
 #include "include/lexer.h"
-#include "include/test.h"
 #include <fstream>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
   Token t;
-  //myfile.open("/tmp/test");
-  //Lexer lex(cin);
   
-  Tests tests();
+  
+  for(int i =1; i < argc; i++){
+	std::string current_exec_name = argv[i];
+	cout << "/tests/lexerTests/" << current_exec_name << " : ";
+	std::ifstream myfile("../../tests/lexerTests/"+current_exec_name); 
+	if(myfile){
+	Lexer lex(myfile);
 
-  //while ((t = lex.nextToken()) != TokenType::ENDF) {
-  //  cout << "->" << t << endl;
-  //}
+	cout << "done\n";
+	
+	while ((t = lex.nextToken()) != TokenType::ENDF) {
+	cout << "->" << t << endl;
+	}
+	}else{
+		cout << "not found\n";
+	}
+
+	
+  }
+  
+  Lexer lex(cin);
+  while ((t = lex.nextToken()) != TokenType::ENDF) {
+		cout << "->" << t << endl;
+  }
+  
   return 0;
 }
