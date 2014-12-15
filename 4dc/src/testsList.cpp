aac6ce43e6085f0331f53c92047a8de1e9e17b12
AubryH <aubry.herve@supelec.fr>
2014-12-15 18:57:41 +0100

    Remove test cpp (we will use a extern module for test)

--- 4dc/src/testsList.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/test.h"
-
-void Tests::loadTestsList(){
-	//dossier de test
-	this->_tests_directory = "tests/lexerTests/";
-
-	//Liste des tests
-	this->add("test1.4d",TestResult::PASS);
-};
