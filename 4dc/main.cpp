377fdac6c5391bec7c99d76277dfc951ded2e3ea
AubryH <aubry.herve@supelec.fr>
2014-12-16 14:11:46 +0100

    correction of indentation in main
    replace tab by double spaces

--- 4dc/main.cpp
+++ 4dc/main.cpp
@@ -12,27 +12,26 @@ int main(int argc, char *argv[])
   
   
   for(int i =1; i < argc; i++){
-	std::string current_exec_name = argv[i];
-	cout << "/tests/lexerTests/" << current_exec_name << " : ";
-	std::ifstream myfile("../../tests/lexerTests/"+current_exec_name); 
-	if(myfile){
-	Lexer lex(myfile);
+    std::string current_exec_name = argv[i];
+    cout << "/tests/lexerTests/" << current_exec_name << " : ";
+    std::ifstream myfile("../../tests/lexerTests/"+current_exec_name); 
+    if(myfile){
+      Lexer lex(myfile);
+	  
+      cout << "done\n";
 
-	cout << "done\n";
-	
-	while ((t = lex.nextToken()) != TokenType::ENDF) {
-	cout << "->" << t << endl;
-	}
-	}else{
-		cout << "not found\n";
-	}
-
-	
+      while ((t = lex.nextToken()) != TokenType::ENDF) {
+        cout << "->" << t << endl;
+      }
+    }else{
+	cout << "not found\n";
+    }
+  
   }
   
   Lexer lex(cin);
   while ((t = lex.nextToken()) != TokenType::ENDF) {
-		cout << "->" << t << endl;
+    cout << "->" << t << endl;
   }
   
   return 0;
