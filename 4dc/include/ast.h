1e855bf730ff82fda7ab669ae3bcdbf9147bd792
Florian Lemaitre <florian@lemaitre.re>
2015-01-11 19:39:56 +0100

    Ajout des AST

---
+++ 4dc/include/ast.h
@@ -0,0 +1,223 @@
+#ifndef AST_H
+#define AST_H
+
+#include <string>
+#include <vector>
+#include <iostream>
+//#include "llvm-dependencies.h"
+
+class Builder;
+
+class AST
+{
+  public:
+    virtual ~AST();
+    template<class T = AST>
+    static T* Error(const std::string& msg);
+  protected:
+  private:
+};
+
+
+template<class T>
+T* AST::Error(const std::string& msg)
+{
+  std::cerr << msg << std::endl;
+  return nullptr;
+}
+class StatementAST;
+class BlocAST : public AST
+{
+  public:
+    BlocAST(StatementAST*);
+    BlocAST(const std::vector<StatementAST*>&);
+    virtual ~BlocAST();
+    //virtual llvm::???* Codegen(Builder&) = 0;
+  protected:
+  private:
+    std::vector<StatementAST*> _statements; // delete at destruction
+};
+
+class StatementAST : public AST
+{
+  public:
+    virtual ~StatementAST();
+    //virtual llvm::???* Codegen(Builder&) = 0;
+  protected:
+  private:
+};
+
+class ExprAST;
+class StatementExprAST : public StatementAST
+{
+  public:
+    StatementExprAST(ExprAST*);
+    virtual ~StatementExprAST();
+    //virtual llvm::???* Codegen(Builder&);
+  protected:
+  private:
+    ExprAST* _expr; // delete at destruction
+};
+
+class IfAST : public StatementAST
+{
+  public:
+    IfAST(ExprAST* condAST, BlocAST* thenAST);
+    IfAST(ExprAST* condAST, BlocAST* thenAST, BlocAST* elseAST);
+    virtual ~IfAST();
+    //virtual llvm::???* Codegen(Builder&);
+  protected:
+  private:
+    ExprAST *_condAST; // delete at destruction
+    BlocAST *_thenAST, *_elseAST; // delete at destruction
+};
+
+class forAST : public StatementAST
+{
+  public:
+    forAST(StatementAST* beginAST, ExprAST* condAST, StatementAST* endAST, BlocAST* loopAST);
+    virtual ~forAST();
+    //virtual llvm::???* Codegen(Builder&);
+  protected:
+  private:
+    StatementAST *_beginAST, *_endAST;
+    ExprAST *_condAST;
+    BlocAST *_loopAST;
+};
+
+class whileAST : public StatementAST
+{
+  public:
+    whileAST(ExprAST* condAST, BlocAST* loopAST);
+    virtual ~whileAST();
+    //virtual llvm::???* Codegen(Builder&);
+  protected:
+  private:
+    ExprAST *_condAST;
+    BlocAST *_loopAST;
+};
+
+class ExprAST : public AST
+{
+  public:
+    virtual ~ExprAST();
+    //virtual llvm::Value* Codegen(Builder&) = 0;
+  protected:
+  private:
+};
+
+class NumberAST : public ExprAST
+{
+  public:
+    NumberAST(double);
+    virtual ~NumberAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+    double _val;
+};
+
+class VariableAST : public ExprAST
+{
+  public:
+    //VariableAST(const std::string &) = 0;
+    virtual ~VariableAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+};
+
+class LocalVariableAST : public ExprAST
+{
+  public:
+    LocalVariableAST(const std::string &);
+    virtual ~LocalVariableAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+    std::string _name;
+};
+
+class GlobaleVariableAST : public ExprAST
+{
+  public:
+    GlobaleVariableAST(const std::string &);
+    virtual ~GlobaleVariableAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+    std::string _name;
+};
+
+class PersistentVariableAST : public ExprAST
+{
+  public:
+    PersistentVariableAST(const std::string &);
+    virtual ~PersistentVariableAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+    std::string _name;
+};
+
+
+class OpAST : public ExprAST
+{
+  public:
+    OpAST(char op, ExprAST* lhs, ExprAST* rhs);
+    virtual ~OpAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+    char _chr;
+    ExprAST *_lhs, *_rhs; // delete at destruction
+};
+
+class CallAST : public ExprAST
+{
+  public:
+    CallAST(const std::string&, const std::vector<ExprAST*>&);
+    virtual ~CallAST();
+    //virtual llvm::Value* Codegen(Builder&);
+  protected:
+  private:
+    std::string _name;
+    std::vector<ExprAST*> _args; // delete at destruction
+};
+
+
+class DefinitionAST : public AST
+{
+  public:
+    virtual ~DefinitionAST() = default;
+    //virtual llvm::Function* Codegen(Builder&) = 0;
+  protected:
+  private:
+};
+
+class PrototypeAST : public DefinitionAST
+{
+  public:
+    PrototypeAST(const std::string&, const std::vector<std::string>&);
+    virtual ~PrototypeAST();
+    //virtual llvm::Function* Codegen(Builder&);
+  protected:
+  private:
+    std::string _name;
+    std::vector<std::string> _args;
+};
+
+class FunctionAST : public DefinitionAST
+{
+  public:
+    FunctionAST(PrototypeAST*, BlocAST*);
+    virtual ~FunctionAST();
+    //virtual llvm::Function* Codegen(Builder&);
+  protected:
+  private:
+    PrototypeAST* _proto; // keep at destruction
+    BlocAST* _body; // delete at destruction
+};
+
+
+#endif // AST_H
---
+++ 4dc/src/ast.cpp
@@ -0,0 +1,390 @@
+#include "../include/ast.h"
+#include <iostream>
+//#include "../include/builder.h"
+
+//using namespace llvm;
+
+/**
+ * AST
+ */
+ AST::~AST() = default;
+
+/**
+ * BlocAST
+ */
+BlocAST::BlocAST(StatementAST* ast)
+  : _statements(1, ast)
+{}
+
+BlocAST::~BlocAST()
+{
+  for (auto& statement : this->_statements) {
+    delete statement;
+  }
+}
+
+/**
+ * StatementAST
+ */
+StatementAST::~StatementAST() = default;
+
+/**
+ * StatementExprAST
+ */
+StatementExprAST::StatementExprAST(ExprAST* expr)
+  : _expr(expr)
+{}
+
+StatementExprAST::~StatementExprAST()
+{
+  delete this->_expr;
+}
+
+/**
+ * IfAST
+ */
+IfAST::IfAST(ExprAST* condAST, BlocAST* thenAST)
+  : IfAST(condAST, thenAST, nullptr)
+{}
+IfAST::IfAST(ExprAST* condAST, BlocAST* thenAST, BlocAST* elseAST)
+  : _condAST(condAST), _thenAST(thenAST), _elseAST(elseAST)
+{}
+
+IfAST::~IfAST()
+{
+  delete this->_condAST;
+  delete this->_thenAST;
+  if (this->_elseAST) delete this->_elseAST;
+}
+
+/*
+Value* IfAST::Codegen(Builder& b)
+{
+  IRBuilder<>& builder = b.irbuilder();
+
+  Value *condV = this->_condAST->Codegen(b);
+
+  if (!condV) {
+    return nullptr;
+  }
+
+  // Convert condition to a bool by comparing equal to 0.0.
+  condV = builder.CreateFCmpONE(
+            condV,
+            ConstantFP::get(getGlobalContext(), APFloat(0.0)),
+            "ifcond");
+
+  Function *f = builder.GetInsertBlock()->getParent();
+
+  // Create blocks for the then and else cases.  Insert the 'then' block at the
+  // end of the function.
+  BasicBlock *thenBB = BasicBlock::Create(getGlobalContext(), "then", f);
+  BasicBlock *elseBB = BasicBlock::Create(getGlobalContext(), "else");
+  BasicBlock *mergeBB = BasicBlock::Create(getGlobalContext(), "ifcont");
+
+  builder.CreateCondBr(condV, thenBB, elseBB);
+
+  // Emit then value.
+  builder.SetInsertPoint(thenBB);
+
+  Value *thenV = this->_thenAST->Codegen(b);
+  if (!thenV) {
+    return nullptr;
+  }
+
+  builder.CreateBr(mergeBB);
+  // Codegen of 'Then' can change the current block, update ThenBB for the PHI.
+  thenBB = builder.GetInsertBlock();
+
+  // Emit else block.
+  f->getBasicBlockList().push_back(elseBB);
+  builder.SetInsertPoint(elseBB);
+
+  Value *elseV = this->_elseAST->Codegen(b);
+  if (!elseV) {
+    return nullptr;
+  }
+
+  builder.CreateBr(mergeBB);
+  // Codegen of 'Else' can change the current block, update ElseBB for the PHI.
+  elseBB = builder.GetInsertBlock();
+
+  // Emit merge block.
+  f->getBasicBlockList().push_back(mergeBB);
+  builder.SetInsertPoint(mergeBB);
+  PHINode *PN = builder.CreatePHI(Type::getDoubleTy(getGlobalContext()), 2,
+                                  "iftmp");
+
+  PN->addIncoming(thenV, thenBB);
+  PN->addIncoming(elseV, elseBB);
+  return PN;
+}
+*/
+
+/**
+ * forAST
+ */
+forAST::forAST(StatementAST* beginAST, ExprAST* condAST, StatementAST* endAST, BlocAST* loopAST)
+  : _beginAST(beginAST), _endAST(endAST), _condAST(condAST), _loopAST(loopAST)
+{}
+
+forAST::~forAST()
+{
+  delete this->_beginAST;
+  delete this->_endAST;
+  delete this->_condAST;
+  delete this->_loopAST;
+}
+
+/**
+ * whileAST
+ */
+whileAST::whileAST(ExprAST* condAST, BlocAST* loopAST)
+  : _condAST(condAST), _loopAST(loopAST)
+{}
+
+whileAST::~whileAST()
+{
+  delete this->_condAST;
+  delete this->_loopAST;
+}
+
+
+/**
+ * ExprAST
+ */
+ExprAST::~ExprAST() = default;
+
+
+/**
+ * NumberAST
+ */
+NumberAST::NumberAST(double n)
+  : _val(n)
+{}
+NumberAST::~NumberAST() = default;
+/*
+Value* NumberAST::Codegen(Builder& b)
+{
+  return ConstantFP::get(getGlobalContext(), APFloat(this->_val));
+}
+*/
+
+/**
+ * VariableAST
+ */
+VariableAST::~VariableAST() = default;
+/*
+Value* VariableAST::Codegen(Builder& b)
+{
+  Value* V = b.namedValues()[this->_name];
+  return V ? V : AST::Error<Value>("Unknown variable name");
+}
+*/
+/**
+ * LocalVariableAST
+ */
+LocalVariableAST::LocalVariableAST(const std::string& name)
+  : _name(name)
+{}
+LocalVariableAST::~LocalVariableAST() = default;
+
+
+/**
+ * GlobaleVariableAST
+ */
+GlobaleVariableAST::GlobaleVariableAST(const std::string& name)
+  : _name(name)
+{}
+GlobaleVariableAST::~GlobaleVariableAST() = default;
+
+
+/**
+ * PersistentVariableAST
+ */
+PersistentVariableAST::PersistentVariableAST(const std::string& name)
+  : _name(name)
+{}
+PersistentVariableAST::~PersistentVariableAST() = default;
+
+
+/**
+ * OpAST
+ */
+OpAST::OpAST(char op, ExprAST* lhs, ExprAST* rhs)
+  : _chr(op), _lhs(lhs), _rhs(rhs)
+{}
+OpAST::~OpAST()
+{
+  delete this->_lhs;
+  delete this->_rhs;
+}
+/*
+Value* OpAST::Codegen(Builder& b)
+{
+
+  Value *L = this->_lhs->Codegen(b);
+  Value *R = this->_rhs->Codegen(b);
+  if (!L || !R) {
+    return nullptr;
+  }
+
+  switch (this->_chr) {
+  case '+':
+    return b.irbuilder().CreateFAdd(L, R, "addtmp");
+  case '-':
+    return b.irbuilder().CreateFSub(L, R, "subtmp");
+  case '*':
+    return b.irbuilder().CreateFMul(L, R, "multmp");
+  case '<':
+    L = b.irbuilder().CreateFCmpULT(L, R, "cmptmp");
+    // Convert bool 0/1 to double 0.0 or 1.0
+    return b.irbuilder().CreateUIToFP(L, Type::getDoubleTy(getGlobalContext()),
+                                      "booltmp");
+  default:
+    return AST::Error<Value>("invalid binary operator");
+  }
+}
+*/
+
+/**
+ * CallAST
+ */
+CallAST::CallAST(const std::string& name, const std::vector<ExprAST*>& args)
+  : _name(name), _args(args)
+{}
+CallAST::~CallAST()
+{
+  auto it = this->_args.begin();
+  auto endIT = this->_args.end();
+  for (; it != endIT ; ++it) {
+    delete *it;
+  }
+  this->_args.clear();
+}
+/*
+Value* CallAST::Codegen(Builder& b)
+{
+  // Look up the name in the global module table.
+  Function *CalleeF = b.module().getFunction(this->_name);
+  if (CalleeF == 0) {
+    return AST::Error<Value>("Unknown function referenced");
+  }
+
+  // If argument mismatch error.
+  if (CalleeF->arg_size() != this->_args.size()) {
+    return AST::Error<Value>("Incorrect # arguments passed");
+  }
+
+  std::vector<Value*> ArgsV;
+
+  auto it = this->_args.begin();
+  auto endIT = this->_args.end();
+  for (; it != endIT; ++it) {
+    ArgsV.push_back((*it)->Codegen(b));
+    if (!ArgsV.back()) {
+      return nullptr;
+    }
+  }
+
+  return b.irbuilder().CreateCall(CalleeF, ArgsV, "calltmp");
+}
+*/
+
+/**
+ * DefinitionAST
+ */
+
+
+
+/**
+ * PrototypeAST
+ */
+PrototypeAST::PrototypeAST(const std::string& name, const std::vector<std::string>& args)
+  : _name(name), _args(args)
+{}
+PrototypeAST::~PrototypeAST() = default;
+/*
+Function* PrototypeAST::Codegen(Builder& b)
+{
+  // Make the function type:  double(double,double) etc.
+  std::vector<Type*> Doubles(this->_args.size(),
+                             Type::getDoubleTy(getGlobalContext()));
+  FunctionType *FT = FunctionType::get(Type::getDoubleTy(getGlobalContext()),
+                                       Doubles, false);
+
+  Function *F = Function::Create(FT, Function::ExternalLinkage, this->_name, &b.module());
+
+
+  // If F conflicted, there was already something named 'Name'.  If it has a
+  // body, don't allow redefinition or reextern.
+  if (F->getName() != this->_name) {
+    // Delete the one we just made and get the existing one.
+    F->eraseFromParent();
+    F = b.module().getFunction(this->_name);
+
+    // If F already has a body, reject this.
+    if (!F->empty()) {
+      return AST::Error<Function>("redefinition of function");
+    }
+
+    // If F took a different number of args, reject.
+    if (F->arg_size() != this->_args.size()) {
+      return AST::Error<Function>("redefinition of function with different # args");
+    }
+  }
+  // Set names for all arguments.
+  auto argsi  = this->_args.begin();
+  auto endi   = this->_args.end();
+  auto fargsi = F->arg_begin();
+  for (; argsi != endi; ++argsi, ++fargsi) {
+    fargsi->setName(*argsi);
+
+    // Add arguments to variable symbol table.
+    b.namedValues()[*argsi] = fargsi;
+  }
+  return F;
+}
+*/
+
+/**
+ * FunctionAST
+ */
+FunctionAST::FunctionAST(PrototypeAST* proto, BlocAST* body)
+  : _proto(proto), _body(body)
+{}
+FunctionAST::~FunctionAST()
+{
+  delete this->_body;
+}
+/*
+Function* FunctionAST::Codegen(Builder& b)
+{
+  b.namedValues().clear();
+
+  Function *f = this->_proto->Codegen(b);
+  if (!f) {
+    return nullptr;
+  }
+
+  // Create a new basic block to start insertion into.
+  BasicBlock *BB = BasicBlock::Create(getGlobalContext(), "entry", f);
+  b.irbuilder().SetInsertPoint(BB);
+  Value *RetVal = this->_body->Codegen(b);
+  if (RetVal) {
+    // Finish off the function.
+    b.irbuilder().CreateRet(RetVal);
+
+    // Validate the generated code, checking for consistency.
+    verifyFunction(*f);
+
+    b.optimize(f);
+
+    return f;
+  }
+
+  // Error reading body, remove function.
+  f->eraseFromParent();
+  return nullptr;
+}
+*/
